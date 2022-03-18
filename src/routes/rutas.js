d449a50070f0a9d1636e6bca7caef20bd70c111d
CarlosMaytorena <carlosmaytorena18@outlook.com>
2022-03-17 23:31:36 -0700

    add

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -6,8 +6,6 @@ const juegos = require('./data.json');
 router.post('/leaderboard', (req,res) => {
     const {usuario, partida, posiciones} = req.body;
     const filtrado = require('./filtrado.json');
-    console.log(juegos.at(0))
-    console.log("indicador")
 
     for(var i1=0;i1<juegos.length;i1++){
         for(var i2=i1+1;i2<juegos.length;i2++){
@@ -22,30 +20,33 @@ router.post('/leaderboard', (req,res) => {
     }
 
     var response = "";
-    var limit = juegos.length;
-    if(posiciones != "" && juegos.length>parseInt(posiciones)){
-        limit=parseInt(posiciones);
-    }
-    console.log(limit)
-
-    for(i1=0;i1<limit;i1++){
+    var cont=0;
+    for(i1=0;i1<juegos.length;i1++){
         if(usuario!=""  && partida!=""){
             if(juegos.at(i1).usuario == usuario && juegos.at(i1).partida == partida){
                 response = response+"\n"+juegos.at(i1).usuario+" | "+juegos.at(i1).partida+" | "+juegos.at(i1).puntaje;
+                cont++;
             }
         }
         else if(usuario!=""){
             if(juegos.at(i1).usuario == usuario){
                 response = response+"\n"+juegos.at(i1).usuario+" | "+juegos.at(i1).partida+" | "+juegos.at(i1).puntaje;
+                cont++;
             }
         }
         else if(partida!=""){
             if(juegos.at(i1).partida == partida){
                 response = response+"\n"+juegos.at(i1).usuario+" | "+juegos.at(i1).partida+" | "+juegos.at(i1).puntaje;
+                cont++;
             }
         }
-        else if(i1<parseInt(posiciones)){
+        else {
             response = response+"\n"+juegos.at(i1).usuario+" | "+juegos.at(i1).partida+" | "+juegos.at(i1).puntaje;
+            cont++;
+        }
+        
+        if(cont==parseInt(posiciones)){
+            i1=juegos.length
         }
     }
 
@@ -55,15 +56,12 @@ router.post('/leaderboard', (req,res) => {
 
 router.get('/report/:usuario/:partida/:puntaje', (req,res) => {
     const {usuario, partida, puntaje} = req.params;
-    console.log(req.params)
-    console.log(usuario)
 
     if(usuario && partida && puntaje){
         const nuevoJuego = {...req.params};
         juegos.push(nuevoJuego);
-        console.log(nuevoJuego);
-        //res.status(200).json(juegos)
-        res.status(200).json("ok")
+        res.status(200).json(juegos)
+        //res.status(200).json("ok")
     }
     else{
         res.status(500).json({error:'no data'})
